a3b4dd04a74951f6414e1c1f3a652ada6f692187
sschatz451 <sebastian@pandh-entertainment.com>
2018-03-05 16:46:08 +0100

    Commit

---
+++ forSchleife.py
@@ -0,0 +1,13 @@
+#einfache For-Schleife
+
+for i in range(1,10):
+  print("Hallo!", end=", ")
+
+name = "Willkommen in HTL Leoben"
+print()
+print(name)
+
+for z in name:
+    print("=", end="")
+for z in name:
+    print("=", end="")
